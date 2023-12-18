51c36457b073abe767658dc3392f059ddf3f4348
Hieu Quang <hquang@unm.edu>
2023-12-18 13:33:06 -0700

    vault backup: 2023-12-18 13:33:06

--- Code/Packet_Passing/packet_server.py
+++ Code/Packet_Passing/packet_server.py
@@ -1,4 +1,9 @@
 import scapy.all as scapy
 
-request = scapy.ARP()
-print(request.show())
+def server_packet_handler(packet):
+    if packet.haslayer(Raw):
+        data = packet[Raw].load.decode('utf-8')
+        print(f"Server received: {data}")
+
+# Start the server and sniff packets
+sniff(prn=server_packet_handler, store=0, filter="tcp port 12345")
