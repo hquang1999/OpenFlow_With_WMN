e3da31b06b5ba9185ecd900682e93ae0b8657d69
Hieu Quang <hquang@unm.edu>
2023-12-19 16:09:12 -0700

    vault backup: 2023-12-19 16:09:12

--- Code/Packet_Passing/packet_server.py
+++ Code/Packet_Passing/packet_server.py
@@ -1,7 +1,11 @@
 import scapy.all as scapy
 
 def process_packet(packet):
-    packet.show()
+    #packet.show()
+    #destination_mac = packet[0][scapy.ETH_P_IP].dst
+    #destination_ip = packet[0][scapy.IP_PROTOS].dst
+    print(scapy.ETH_P_IP)
+
 
 traffic = scapy.sniff(iface="wlp2s0", prn=process_packet, count=1)
 '''
