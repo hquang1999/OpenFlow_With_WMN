7516da69f42e7a09f8f5d5d18ec1a600ffc641a7
Hieu Quang <hquang@unm.edu>
2023-12-19 14:58:20 -0700

    vault backup: 2023-12-19 14:58:20

--- Code/Packet_Passing/packet_server.py
+++ Code/Packet_Passing/packet_server.py
@@ -1,5 +1,10 @@
-import scapy.all as scapy
+from scapy.all import *
 
+packet = IP(dst='192.168.1.15')/ICMP()
+send(packet)
+
+'''
 traffic = scapy.sniff(iface="wlp2s0", count=5)
 traffic.nsummary()
+'''
 
