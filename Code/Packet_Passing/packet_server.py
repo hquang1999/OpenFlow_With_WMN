5a60cd7c22e074d6d77d9d7aca7da70df19d5a83
Hieu Quang <hquang@unm.edu>
2023-12-26 20:08:27 -0700

    vault backup: 2023-12-26 20:08:27

--- Code/Packet_Passing/packet_server.py
+++ Code/Packet_Passing/packet_server.py
@@ -1,16 +1,8 @@
-import scapy.all as scapy
+import socket
+import threading as thread
 
-def process_packet(packet):
-    if (packet.haslayer(scapy.ICMP)):
-        destination_mac = packet[0][0].src
-        destination_ip = packet[0][1].src
-        print(f"dst_IP = {destination_ip} | dst_MAC = {destination_mac}")
+PORT = 5500
+#SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = "192.168.1.113"
 
-traffic = scapy.sniff(iface="wlp2s0", prn=process_packet, count=10)
-
-test = scapy.packet
-'''
-traffic = scapy.sniff(iface="wlp2s0", count=5)
-traffic.nsummary()
-'''
-
+print(SERVER)
