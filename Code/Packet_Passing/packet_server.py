1759d0141f501cda13a223a1f21084e384e730a5
Hieu Quang <hquang@unm.edu>
2023-12-26 20:23:27 -0700

    vault backup: 2023-12-26 20:23:27

--- Code/Packet_Passing/packet_server.py
+++ Code/Packet_Passing/packet_server.py
@@ -1,8 +1,35 @@
-import socket
-import threading as thread
+import socket as sck
+import threading
 
 PORT = 5500
-#SERVER = socket.gethostbyname(socket.gethostname())
-SERVER = "192.168.1.113"
+SERVER_IP = "192.168.1.113"
+ADDR = (SERVER_IP, PORT)
 
-print(SERVER)
+# Creates the socket
+server = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+# Binds the socket to the ip and port #
+server.bind(ADDR)
+
+# Individual connection handler
+def client_handler(connected_obj, address):
+    pass
+
+def server_side_handler():
+    server.listen()
+    while True:
+        # This line is waiting for a new connection to the server
+        # (object, IP and port)
+        connected_obj, address = server.accept()
+        # Thread for the client handler
+        thread = threading.Thread(target=client_handler(),
+                                  args=(connected_obj, address))
+        thread.start()
+        # Prints out active client threads
+        print(f"[Active Connection] {threading.activeCount() - 1}")
+
+
+
+print("[Starting] server socket is starting!")
+server_side_handler()
+
+
