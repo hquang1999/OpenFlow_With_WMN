cbe8d31ed27feab37da974863216149dd62f7e8d
Hieu Quang <hquang@unm.edu>
2023-12-26 19:58:27 -0700

    vault backup: 2023-12-26 19:58:27

--- Code/Packet_Passing/packet_server.py
+++ Code/Packet_Passing/packet_server.py
@@ -1,13 +1,14 @@
 import scapy.all as scapy
 
 def process_packet(packet):
-    packet.show()
-    destination_mac = packet[0][0].src
-    destination_ip = packet[0][1].src
-    print(f"dst_IP = {destination_ip} | dst_MAC = {destination_mac}")
+    if (packet.haslayer(scapy.ICMP)):
+        destination_mac = packet[0][0].src
+        destination_ip = packet[0][1].src
+        print(f"dst_IP = {destination_ip} | dst_MAC = {destination_mac}")
 
+traffic = scapy.sniff(iface="wlp2s0", prn=process_packet, count=10)
 
-traffic = scapy.sniff(iface="wlp2s0", prn=process_packet, count=1)
+test = scapy.packet
 '''
 traffic = scapy.sniff(iface="wlp2s0", count=5)
 traffic.nsummary()
